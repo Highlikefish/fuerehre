eef4564384fde038f6a1695d220694026b27b5b6
highlikefish <joejackhugo@gmail.com>
2022-04-02 14:14:11 +0200

    edited Doppelklang.py    - überprüfung der Eingabe

---
+++ gigacalc.py
@@ -0,0 +1,18 @@
+# Reihe () -> print anzahl kronenkorken
+Kronkorken = list((0,1,))
+Reihe = list(("0","1","4","10","19",))
+
+i: int = 1
+a: int = 1
+while i == 1 :
+    while a <= 100 :
+        Kronkorken: object
+        z: int = Kronkorken[-1]
+        z + 3 * a = K
+        Kronkorken.append(K)
+        var = a + 1
+        print(a)
+    else:
+        i = 0
+else:
+    print("ich habe fertig")
